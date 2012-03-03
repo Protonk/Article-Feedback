f0c91ac562e57c5e4e330c88b2d1860a878c582e
Adam Hyland <protonk@gmail.com>
2012-03-03 10:45:01 -0600

    Add option for initial run (pulls remote files and creates local versions).
    Cleanup some code
    
    slight speed improvement for read.csv

--- feed.import.R
+++ feed.import.R
@@ -3,144 +3,155 @@
 # Adding the exact file path, of course. 
 
 
-## Function to grab local data or remote. Defaults to local
-## Your local path will be different
 
-buildFeedDf <- function(remote = FALSE) {
-  grabWikiFeed <- function() {
-    aap.url <- "http://dumps.wikimedia.org/other/articlefeedback/aa_combined-20110321.csv.gz"
-    temp <- tempfile()
-    download.file(aap.url, temp)
-    aap.file <- read.csv(gzfile(temp), as.is = TRUE)
-    unlink(temp)
-    return(aap.file)
-  }
-  if (remote) grabWikiFeed()
-  else read.csv("/Users/protonk/R/Dropbox/Article-Feedback/Articles/afdump.csv", as.is= TRUE)
+
+importFeedDb <- function(ingest) {
+	project.dir <- getwd()
+	buildFeedDf <- function(ingest) {
+		aap.classes <- c("numeric", "numeric", "character", rep("numeric", 9))
+		grabRemote <- function(create.dir = FALSE) {
+			temp <- tempfile()
+			download.file("http://dumps.wikimedia.org/other/articlefeedback/aap_combined-20110919.csv.gz", temp)
+			aap.file <- read.csv(gzfile(temp), colClasses = aap.classes, nrows = 799641)
+			unlink(temp)
+			if (create.dir) {
+				dir.create(paste(project.dir, "Articles", sep= "/"))
+				write.csv(aap.file, paste(project.dir, "Articles",  "afdump.csv", sep = "/"));
+			}
+			return(aap.file)
+		}
+		switch(ingest,
+			local = read.csv(paste(project.dir, "Articles", "afdump.csv", sep = "/"), colClasses = aap.classes, nrows = 799641),
+			remote = grabRemote(),
+			initial = grabRemote(create.dir = TRUE)
+			)
+	}
+	
+	
+	# Generate character vectors for "rated" articles and use them to create
+	# factors in the feedback dataframe. Local or remote. 
+	
+	applyFactors <- function(ingest) {
+		# Enumerate categories from the Mediawiki API
+		
+		fetchCompleteCat <- function(category, namespace = 0) {
+			# This is NOT rjson 0.2.6. I have changed it so the internal call to readLines doesn't toss warngings
+			# Also RJSONIO will mangle special characters like —, don't use it
+			library(rjson)
+			# Mediawiki requires an informative user agent. Yours should be different
+			options(HTTPUserAgent="Category Enumeration by User:Protonk on en.wp, R 2.14.1")
+			fetchCategoryURL <- function(category, namespace, continue = NULL) {
+				url.start <-paste("http://en.wikipedia.org/w/api.php?", 
+								  "action=query",
+								  "list=categorymembers",
+								  paste("cmtitle", "=", category, sep = ""),
+								  paste("cmnamespace", "=", namespace, sep = ""),
+								  "cmlimit=500",
+								  "cmtype=page",
+								  "cmprop=title",
+								  "format=json",
+								  sep = "&")
+				if (is.null(continue)) url.gen <- url.start
+				else url.gen <- paste(url.start, paste("cmcontinue", "=", continue, sep = ""), sep = "&")
+				url.gen
+			}
+			cat.list.init <- fromJSON(file = fetchCategoryURL(category, namespace))
+			cat.unform <- unlist(cat.list.init$query$categorymembers)[names(unlist(cat.list.init$query$categorymembers)) == "title"]
+			while (!is.null(cat.list.init$`query-continue`)) {
+				cat.list.init <- fromJSON(file = fetchCategoryURL(category, namespace, continue = unlist(cat.list.init$`query-continue`)[[1]]))
+				cat.unform <- append(cat.unform, unlist(cat.list.init$query$categorymembers)[names(unlist(cat.list.init$query$categorymembers)) == "title"])
+			}
+			cat.final <- unname(cat.unform)
+			closeAllConnections()
+			return(cat.final)
+		}
+	
+		charvec.names <- c("formerGA.txt",
+						   "formerFA.txt",
+						   "fmrGAnom.txt",
+						   "GA.txt",
+						   "FL.txt",
+						   "FA.txt")
+						   
+		local.articles <- paste(project.dir, "Articles", charvec.names, sep = "/")
+		
+		remote.articles <- c("Category:Delisted_good_articles",
+							 "Category:Wikipedia_former_featured_articles",
+							 "Category:Former_good_article_nominees",
+							 "Category:Good_articles",
+							 "Category:Featured_lists",
+							 "Category:Featured_articles") 
+							 
+		factor.labels <- c("Former Good Article",
+						   "Former Featured Article",
+						   "Good Article Nominee",
+						   "Good Article",
+						   "Featured List",
+						   "Featured Article") 
+						   
+		list.out <- vector("list", 6)
+		names(list.out) <- charvec.names
+								   
+		remoteFactors <- function(create.dir = FALSE) {
+			namespace <- 1
+			for (i in seq_along(remote.articles)) {
+				# First three are actually talk page categories
+				# in any case, restricting by namespace removes some erroneously tagged pages
+				if (i > 3) namespace <- 0 
+				list.out[[i]] <- fetchCompleteCat(category = remote.articles[i], namespace = namespace)
+			}
+			#Some have the category transcluded on an archive page
+			#The rest need "Talk:" stripped out 
+			list.out <- lapply(list.out, gsub, pattern = "(Talk:)|(/Archive)(.*)", replacement = "")
+			if (create.dir) sapply(names(list.out), function (x) write(paste(project.dir, "Articles", list.out[[x]], sep = "/"), file= x ))
+			list2env(list.out, envir = parent.frame())
+		}
+		
+		localFactors <- function() {
+			for (x in seq_along(local.articles)) {
+				assign(charvec.names[x], as.character(read.delim(local.articles[x], sep = "\n", header = FALSE, as.is= TRUE)[,1]), envir = parent.frame())
+			}
+		}
+		
+		switch(ingest,
+			   local = localFactors(),
+			   remote = remoteFactors(),
+			   initial = remoteFactors(create.dir = TRUE)
+			   )
+			   
+		feed.df[feed.df[,"title"] %in% formerGA.txt, "Assessment"] <<- "Former Good Article"
+		feed.df[feed.df[,"title"] %in% formerFA.txt, "Assessment"] <<- "Former Featured Article"
+		feed.df[feed.df[,"title"] %in% fmrGAnom.txt, "Assessment"] <<- "Good Article Nominee"
+		feed.df[feed.df[,"title"] %in% GA.txt, "Assessment"] <<- "Good Article"
+		feed.df[feed.df[,"title"] %in% FL.txt, "Assessment"] <<- "Featured List"
+		feed.df[feed.df[,"title"] %in% FA.txt, "Assessment"] <<- "Featured Article"
+	}
+	
+	# Drops namespace and page id
+	# in the future, page id will be returned to the df but for now lists of pertinent articles are generated by 
+	# article title. 
+	
+	feed.df <- buildFeedDf(ingest)[, c(3:12)]
+	names(feed.df) <- c("title", "length", "total_wellsourced", "count_wellsourced", "total_neutral", "count_neutral", "total_complete", "count_complete", "total_readable", "count_readable")
+	
+	# Titles grabbed from the mediawiki API will not have the underscore. 
+	feed.df[, "title"] <- gsub(pattern = "_",replacement = " ", feed.df[, "title"])
+	
+	# Create factor variable for Project Assessment and assign it with applyFactors()
+	
+	feed.df$Assessment<- factor(x = c("Unassessed"), levels=c("Unassessed", "Former Good Article", "Former Featured Article", "Good Article Nominee",  "Good Article", "Featured List", "Featured Article"))
+	
+	applyFactors(ingest)
+	feed.df
 }
 
-# This is NOT rjson 0.2.6. I have changed it so the internal call to readLines doesn't toss warngings
-# Also RJSONIO will mangle special characters like —, don't use it
-
-library(rjson)
-
-# Enumerate categories from the Mediawiki API
-
-fetchCompleteCat <- function(category, namespace = 0) {
-  # Mediawiki requires an informative user agent. Yours should be different
-  options(HTTPUserAgent="Category Enumeration by User:Protonk on en.wp, R 2.14.1")
-  fetchCategoryURL <- function(category, namespace, continue = NULL) {
-    url.start <-paste(
-      "http://en.wikipedia.org/w/api.php?", 
-      "action=query",
-      "list=categorymembers",
-      paste("cmtitle", "=", category, sep = ""),
-      paste("cmnamespace", "=", namespace, sep = ""),
-      "cmlimit=500",
-      "cmtype=page",
-      "cmprop=title",
-      "format=json",
-      sep = "&")
-    if (is.null(continue)) url.gen <- url.start
-    else url.gen <- paste(url.start, paste("cmcontinue", "=", continue, sep = ""), sep = "&")
-    url.gen
-  }
-  cat.list.init <- fromJSON(file = fetchCategoryURL(category, namespace))
-  cat.unform <- unlist(cat.list.init$query$categorymembers)[names(unlist(cat.list.init$query$categorymembers)) == "title"]
-  while (!is.null(cat.list.init$`query-continue`)) {
-    cat.list.init <- fromJSON(file = fetchCategoryURL(category, namespace, continue = unlist(cat.list.init$`query-continue`)[[1]]))
-    cat.unform <- append(cat.unform, unlist(cat.list.init$query$categorymembers)[names(unlist(cat.list.init$query$categorymembers)) == "title"])
-  }
-  cat.final <- unname(cat.unform)
-  closeAllConnections()
-  return(cat.final)
-}
-
-
-# Generate character vectors for "rated" articles and use them to create
-# factors in the feedback dataframe. Local or remote. 
-
-applyFactors <- function(remote = FALSE) {
-  local.articles <- c("/Users/protonk/r/Dropbox/Article-Feedback/Articles/formerGA.txt", 
-                      "/Users/protonk/r/Dropbox/Article-Feedback/Articles/formerFA.txt",
-                      "/Users/protonk/r/Dropbox/Article-Feedback/Articles/fmrGAnom.txt", 
-                      "/Users/protonk/r/Dropbox/Article-Feedback/Articles/GA.txt",
-                      "/Users/protonk/r/Dropbox/Article-Feedback/Articles/FL.txt", 
-                      "/Users/protonk/r/Dropbox/Article-Feedback/Articles/FA.txt")
-  remote.articles <- c("Category:Delisted_good_articles",
-                       "Category:Wikipedia_former_featured_articles",
-                       "Category:Former_good_article_nominees",
-                       "Category:Good_articles",
-                       "Category:Featured_lists",
-                       "Category:Featured_articles")
-  charvec.names <- c("formerGA.list",
-                     "formerFA.list",
-                     "nomGA.list",
-                     "GA.list",
-                     "FL.list",
-                     "FA.list")
-
-# This is the function that grabs all the categories (calling fetchCompleteCat)
-  
-  remoteFactors <- function() {
-    namespace <- numeric()
-    list.out <- vector("list", 6)
-    names(list.out) <- charvec.names
-    for (i in seq_along(remote.articles)) {
-      # First three are actually talk page categories
-      # in any case, restricting by namespace removes some erroneously tagged pages
-      if (i <= 3) namespace <- 1 
-      else { 
-        namespace <- 0
-      }
-      list.out[[i]] <- fetchCompleteCat(category = remote.articles[i], namespace = namespace)
-      if (i <= 3) {
-        #Some have the category transcluded on an archive page
-        #The rest need "Talk:" stripped out 
-        list.out[[i]] <- gsub("(/Archive)(.*)", replacement = "", list.out[[i]])
-        list.out[[i]] <- gsub(pattern = "Talk:", replacement = "", list.out[[i]])
-      }
-    }
-    return(list.out)
-  }
-  if (remote) {
-    comb.list <- remoteFactors()
-    # Builds 6 individual character vectors. Easiest for assignment to the DF
-    list2env(comb.list, envir = parent.frame())
-  }
-  else {
-    for (x in seq_along(local.articles)) {
-      assign(charvec.names[x], as.character(read.delim(local.articles[x], sep = "\n", header = FALSE, as.is= TRUE)[,1]))
-    }
-  }
-  feed.df[feed.df[,"title"] %in% formerGA.list, "Assessment"] <<- "Former Good Article"
-  feed.df[feed.df[,"title"] %in% formerFA.list, "Assessment"] <<- "Former Featured Article"
-  feed.df[feed.df[,"title"] %in% nomGA.list, "Assessment"] <<- "Good Article Nominee"
-  feed.df[feed.df[,"title"] %in% GA.list, "Assessment"] <<- "Good Article"
-  feed.df[feed.df[,"title"] %in% FL.list, "Assessment"] <<- "Featured List"
-  feed.df[feed.df[,"title"] %in% FA.list, "Assessment"] <<- "Featured Article"
-}
-
-# Drops namespace and page id
-# in the future, page id will be returned to the df but for now lists of pertinent articles are generated by 
-# article title. 
-
-feed.df <- buildFeedDf()[, c(3:12)]
-names(feed.df) <- c("title", "length", "total_wellsourced", "count_wellsourced", "total_neutral", "count_neutral", "total_complete", "count_complete", "total_readable", "count_readable")
+feed.df <- importFeedDb(ingest = "local")
 
 # Compute rating/count sums
 
 feed.df$sum_ratings <- feed.df[, "total_wellsourced"] + feed.df[, "total_neutral"] + feed.df[, "total_complete"] + feed.df[, "total_readable"]
 feed.df$sum_count <- feed.df[, "count_wellsourced"] + feed.df[, "count_neutral"] + feed.df[, "count_complete"] + feed.df[, "count_readable"]
 feed.df$rating_avg <- feed.df[, "sum_ratings"] / feed.df[, "sum_count"]
-
-# Create factor variable for Project Assessment and assign it with applyFactors()
-
-feed.df$Assessment<- factor(x = c("Unassessed"), levels=c("Unassessed", "Former Good Article", "Former Featured Article", "Good Article Nominee",  "Good Article", "Featured List", "Featured Article"))
-# Titles grabbed from the mediawiki API will not have the underscore. 
-feed.df[, "title"] <- gsub(pattern = "_",replacement = " ", feed.df[, "title"])
-applyFactors()
-
 
 # Drop rows with:
 ##  50 length or less. < 0 length are erroneous values, 0-50 tend to be redirects
@@ -212,5 +223,5 @@ rownames(feed.rated) <- 1:nrow(feed.rated)
 
 # precorMatrix is removed because the rows it depends upon are removed
 
-remove(unclean.rows, buildFeedDf, applyFactors, precorMatrix, fetchCompleteCat, feed.small, i)
+remove(unclean.rows, precorMatrix, feed.small, i, importFeedDb)
 closeAllConnections()
