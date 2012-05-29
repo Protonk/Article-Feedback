7776d28baae4eb24a78250ab2c2ba2674a19f1a8
Adam Hyland <protonk@gmail.com>
2012-05-29 18:38:19 -0500

    simplify bar chart.
    properly create toplist
    
    remove test bits.

--- quick.ratings.R
+++ quick.ratings.R
@@ -1,46 +1,32 @@
 library(ggplot2)
 
-toplist <- head(order(tabulate(indrat[, "Page_ID"]), decreasing = TRUE), 1000)
-# Lazy way to get a top list of rated articles
-toptitles <- unique(indrat[indrat[, "Page_ID"] %in% toplist, c("Page_ID", "Title")])
+# build dfs for ranking and for pages in those rankings
+# merge to preserve order
+# top 1000 chosen
+toplist <- data.frame(Page_ID = head(order(tabulate(indrat[, "Page_ID"]), decreasing = TRUE), 1000),
+                      Rank = 1:1000)
+toptitles <- unique(indrat[indrat[, "Page_ID"] %in% toplist[, "Page_ID"], c("Page_ID", "Title")])
+topfinal <- merge(toplist, toptitles, by = "Page_ID")
+# Sort for easy viewing w/ head()
+topfinal <- topfinal[order(topfinal[, "Rank"]), ]
 
-#  ddply(subset(indrat, Page_ID == 23680998), .(Date), "nrow")
 
-with(ddply(subset(indrat, Page_ID == 23680998), .(Date), "nrow"), plot(Date, seq(1, 7, length.out = 156), type = "n", frame.plot=FALSE))
-
-for (i in 1:6) {
-  with(ddply(subset(indrat, Page_ID == toplist[i]), .(Date), "nrow"), lines(Date, log(pmax(nrow, 1)), type = "l", col = i))
+aveByRating <- function(data = indrat, all = TRUE) {
+  # We are only interested in rows where the user rated all 4 categories
+  rating.avgs <- data[data[, "Rated All"] == all, "Mean"] 
+  count.table <- table(rating.avgs)
+  # Builds factors of the rating avgs (ordered by count)
+  # and another factor for just the integers (this is a ggplot2 thing)
+  count.out <- factor(rating.avgs, levels = names(count.table), ordered = TRUE)
+  integers <- factor(count.out, levels = as.character(1:5))
+  # placed into a data frame for easier plotting
+  data.frame(count.out, integers)
 }
-
-
-brevik <- ddply(subset(indrat, Page_ID == 32501324), .(Date), summarise, mean = mean(Mean))
-brevik[, "Ratings"] <-  ddply(subset(indrat, Page_ID == 32501324), .(Date), "nrow")[, 2]
-
-with(brevik, plot(Date, mean, col = "blue", pch = 20, ylim = c(0,6), frame.plot = FALSE, type = "b", cex = log(brevik[, "Ratings"]) + 1))
-
-bieber <- ddply(subset(indrat, Page_ID == 23680998), .(Date), summarise, mean = mean(Mean))
-bieber[, "Ratings"] <-  ddply(subset(indrat, Page_ID == 23680998), .(Date), "nrow")[, 2]
-
-with(bieber, plot(Date, mean, col = "blue", pch = 20, ylim = c(0,6), frame.plot = FALSE, type = "b", cex = log(bieber[, "Ratings"]) + 1))
-
-
-# We are only interested in rows where the user rated all 4 categories
-rating.avgs <- rowMeans(reduced[complete.cases(reduced), ])
-
-count.table <- table(rating.avgs)
-
-# Builds factors of the rating avgs (ordered by count)
-# and another factor for just the integers (this is a ggplot2 thing)
-count.out <- factor(rating.avgs, levels = names(count.table), ordered = TRUE)
-integers <- factor(count.out, levels = as.character(1:5))
-# placed into a data frame for easier plotting
-preplot <- data.frame(count.out, integers)
-
+fourcat.table <- aveByRating(data = indrat, all = TRUE)
 # Plot frequency of averages
-qplot(count.out, fill = integers, geom = "bar", data = preplot) + 
+qplot(count.out, fill = integers, geom = "bar", data = fourcat.table) + 
   opts(legend.position = "none", title = expression("Averages of ratings where users rated all four categories")) + 
   scale_y_continuous(name = "") +  scale_x_discrete(name = "")
-
 
 
 
